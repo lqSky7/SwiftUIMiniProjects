ace11eafbc599d4125ed1d1fe69c2f03decd9f37
ca5 <catinice@outlook.com>
2025-12-05 11:59:02 +0530

    bestGradient
    Signed-off-by: ca5 <catinice@outlook.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,31 +7,16 @@
 
 import SwiftUI
 
-enum AppData{
-    static var points: [SIMD2<Float>] = [
-        [0, 0], [0.5, 0], [1.0, 0],
-        [0, 0.5], [0.5, 0.5], [1.0, 0.5],
-        [0, 1.0],[0.5, 1.0],[1.0, 1.0],
-    ]
-    
-    static var Colorr : [Color] = [
-        .black, .pink, .red, .yellow
-    ]
-    
-    static func RandomColors() -> [Color] {
-        (0...8).map {_ in Colorr.randomElement()!}
-    }
-}
-
 struct ContentView: View {
    
-    @State private var col = AppData.RandomColors();
+
     var body: some View{
         NavigationStack{
-            MeshGradient(width: 3, height: 3, points: AppData.points, colors: col)
-            .navigationTitle("Mesh.")
-            .navigationBarTitleDisplayMode(.automatic)
-            .ignoresSafeArea(edges: .bottom)
+            
+                Text("HII")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.indigo.gradient)
+            
         }
         
     }
