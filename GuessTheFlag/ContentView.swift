32bf1c9912b66438ac57994e3da0ae6e5a1d18dd
ca5 <catinice@outlook.com>
2025-12-05 23:19:07 +0530

    initial impl of project
    Signed-off-by: ca5 <catinice@outlook.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,18 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAL = false
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
+                     "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var correctAns = Int.random(in: 0...2)
+    @State var showResult = false
+    @State var result = ""
     
     var body: some View{
-        Button("Show alrt"){
-            showingAL = true
-        }.alert("Important", isPresented: $showingAL){
-            Button("OK", role: .confirm){}
-            Button("REPORT", role: .destructive) {}
+        ZStack{
+            RadialGradient(colors: [.pink,.black], center: .center, startRadius: 0, endRadius: 340).ignoresSafeArea()
+            VStack(spacing: 30){
+                HStack{
+                    Text("Tap the flag of").italic().foregroundColor(.white)
+                    Text(countries[correctAns]).bold().foregroundColor(.white)
+                }
+                ForEach(0..<3){
+                    number in
+                    Button{
+                        flagTapped(curr: number)
+                        
+                    }
+                    label: {Image(countries[number])
+                    }
+                    
+                }
+                
+            }
+            
+        }.alert(result , isPresented: $showResult){
+            Button("Continue", action: shuff)
+            // FIX: SHOW SCORE USER
         }
+    }
+    func flagTapped(curr: Int){
+        if(curr == correctAns){
+            showResult = true
+            result = "Correct!"
+        } else {
+            showResult = true
+            result = "False!"
+        }
+    }
+    
+    func shuff(){
+        countries.shuffle()
+        correctAns = Int.random(in: 0...2)
     }
 }
 
 #Preview {
     ContentView()
 }
+
