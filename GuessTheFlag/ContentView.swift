bde764d0e4146784b730ef0f9e75dfd22f8763e6
ca5 <catinice@outlook.com>
2025-12-05 10:50:26 +0530

    spacer and stacks
    Signed-off-by: ca5 <catinice@outlook.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Spacer()
             Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Spacer()
+            Spacer()
         }
         .padding()
     }
